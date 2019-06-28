8764e3ab2ce5ab70e6eca70125424547c38ba305
Luis Fernando Raga <whary11@gmail.com>
2019-06-28 11:35:24 -0500

    ....

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@php env('APP_NAME') @endphp</title>
+    <title> {{env('APP_NAME')}} </title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css">
    
