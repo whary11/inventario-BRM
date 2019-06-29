8d1f2760f39d71cebfe850a6a4af24ff3e663fc1
Luis Fernando Raga <whary11@gmail.com>
2019-06-28 23:29:04 -0500

    Algunos cambios en los controladores...

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -20,25 +20,25 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/"> {{env('APP_NAME')}} </a>
+        <a class="navbar-brand text-primary" href="/"> {{env('APP_NAME')}} </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent" >
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                 <li class="nav-item active">
                 </li>
                 <li class="nav-item dropdown my-2 my-lg-0">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    <a class="nav-link dropdown-toggle text-primary" href="#" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Basicos
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="productos">Nuevo producto</a>
+                        <a class="dropdown-item text-primary" href="productos">Nuevo producto</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="/productos_proveedor">Proveer productos</a>
+                        <a class="dropdown-item text-primary" href="/productos_proveedor">Proveer productos</a>
                     </div>
 
 
@@ -46,17 +46,15 @@
 
 
                 <li class="nav-item">
-                    <a class="nav-link" href="/comprar" tabindex="-1" aria-disabled="true">Comprar</a>
+                    <a class="nav-link text-primary" href="/comprar" tabindex="-1" aria-disabled="true">Comprar</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                    <a class="nav-link dropdown-toggle text-primary" href="#" id="navbarDropdown" role="button"
                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         Consultar
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        {{-- <a class="dropdown-item" href="#">Productos en inventario</a>
-                            <div class="dropdown-divider"></div> --}}
-                        <a class="dropdown-item" href="/hisorial">Historial de facturas</a>
+                        <a class="dropdown-item text-primary" href="/hisorial">Facturas</a>
                     </div>
                 </li>
             </ul>
